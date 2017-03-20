57c37865989af4cca331dbb7d5401511720b1557
Sean Sobey <Sean.M.Sobey@gmail.com>
2017-03-20 10:07:02 +0200

    More WIP excel

--- xUnitExtensions/ExcelDataAttribute.cs
+++ xUnitExtensions/ExcelDataAttribute.cs
@@ -1,91 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.OleDb;
-using System.IO;
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Linq;
-using System.Reflection;
-using Xunit.Sdk;
 
 namespace Xunit
 {
-	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-	public sealed class ExcelDataAttribute : DataAttribute
+	public class ExcelDataAttribute
 	{
-		readonly static string connectionTemplate = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source={0}; Extended Properties='Excel 12.0;HDR=YES;IMEX=1;';";
-		readonly string _fileName;
-		readonly string _queryString;
-
-		public ExcelDataAttribute(string fileName, string queryString)
+		public ExcelDataAttribute(string filepath, string sheetName, string addressName)
 		{
-			_fileName = fileName;
-			_queryString = queryString;
-		}
-
-		public override IEnumerable<object[]> GetData(MethodInfo testMethod)
-		{
-			if (testMethod == null)
-				throw new ArgumentNullException(nameof(testMethod));
-
-			ParameterInfo[] pars = testMethod.GetParameters();
-			return DataSource(_fileName, _queryString, pars.Select(par => par.ParameterType).ToArray());
-		}
-
-		IEnumerable<object[]> DataSource(string fileName, string selectString, Type[] parameterTypes)
-		{
-			string connectionString = string.Format(connectionTemplate, GetFullFilename(fileName));
-			IDataAdapter adapter = new OleDbDataAdapter(selectString, connectionString);
-			DataSet dataSet = new DataSet();
-
-			try
+			using (var spreadsheetDocument = SpreadsheetDocument.Open(filepath, false))
 			{
-				adapter.Fill(dataSet);
+				// Retrieve a reference to the workbook part.
+				var workbookPart = spreadsheetDocument.WorkbookPart;
 
-				foreach (DataRow row in dataSet.Tables[0].Rows)
-					yield return ConvertParameters(row.ItemArray, parameterTypes);
-			}
-			finally
-			{
-				IDisposable disposable = adapter as IDisposable;
-				disposable.Dispose();
+				// Find the sheet with the supplied name, and then use that 
+				// Sheet object to retrieve a reference to the first worksheet.
+				var sheet = workbookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == sheetName).FirstOrDefault();
+
+				// Throw an exception if there is no sheet.
+				if (sheet == null)
+					throw new ArgumentException("sheetName");
+
+				// Retrieve a reference to the worksheet part.
+				var worksheetPart = workbookPart.GetPartById(sheet.Id) as WorksheetPart;
+
+				// For shared strings, look up the value in the
+				// shared strings table.
+				var sharedStringTablePart = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+				// If the shared string table is missing, something 
+				// is wrong. Return the index that is in
+				// the cell. Otherwise, look up the correct text in 
+				// the table.
+				if (sharedStringTablePart == null)
+					throw new Exception();
+
+				// Use its Worksheet property to get a reference to the cell 
+				// whose address matches the address you supplied.
+				var cell = worksheetPart.Worksheet.Descendants<Cell>().Where(c => c.CellReference == addressName).FirstOrDefault();
+
 			}
 		}
 
-		static string GetFullFilename(string filename)
+		static object ParseCell(Cell cell, SharedStringTablePart sharedStringTablePart)
 		{
-			string executable = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-			return Path.GetFullPath(Path.Combine(Path.GetDirectoryName(executable), filename));
-		}
-
-		static object[] ConvertParameters(object[] values, Type[] parameterTypes)
-		{
-			object[] result = new object[values.Length];
-
-			for (int idx = 0; idx < values.Length; idx++)
-				result[idx] = ConvertParameter(values[idx], idx >= parameterTypes.Length ? null : parameterTypes[idx]);
-
-			return result;
-		}
-
-		/// <summary>
-		/// Converts a parameter to its destination parameter type, if necessary.
-		/// </summary>
-		/// <param name="parameter">The parameter value</param>
-		/// <param name="parameterType">The destination parameter type (null if not known)</param>
-		/// <returns>The converted parameter value</returns>
-		static object ConvertParameter(object parameter, Type parameterType)
-		{
-			if ((parameter is double || parameter is float) &&
-				(parameterType == typeof(int) || parameterType == typeof(int?)))
+			string value = null;
+			// If the cell does not exist, return an empty string.
+			if (cell == null)
 			{
-				int intValue;
-				string floatValueAsString = parameter.ToString();
+				value = cell.InnerText;
 
-				if (Int32.TryParse(floatValueAsString, out intValue))
-					return intValue;
+				// If the cell represents an integer number, you are done. 
+				// For dates, this code returns the serialized value that 
+				// represents the date. The code handles strings and 
+				// Booleans individually. For shared strings, the code 
+				// looks up the corresponding value in the shared string 
+				// table. For Booleans, the code converts the value into 
+				// the words TRUE or FALSE.
+				if (cell.DataType != null)
+				{
+					switch (cell.DataType.Value)
+					{
+						case CellValues.Boolean:
+							return value == "1";
+						case CellValues.Number:
+							break;
+						case CellValues.Error:
+							break;
+						case CellValues.SharedString:
+							value = sharedStringTablePart.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+							break;
+						case CellValues.String:
+							break;
+						case CellValues.InlineString:
+							break;
+						case CellValues.Date:
+							break;
+						default:
+							break;
+					}
+				}
 			}
-
-			return parameter;
 		}
 	}
 }
