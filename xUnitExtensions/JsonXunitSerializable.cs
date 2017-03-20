931736f0847e76bd0036030453dc8efdf2428f2e
Sean Sobey <Sean.M.Sobey@gmail.com>
2017-03-20 17:23:06 +0200

    Added WIP JsonXunitSerializable.

---
+++ xUnitExtensions/JsonXunitSerializable.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using Xunit.Abstractions;
+
+namespace Xunit
+{
+	public class JsonXunitSerializable<T> : IXunitSerializable where T : new()
+	{
+		T _obj;
+
+		public JsonXunitSerializable(T obj)
+		{
+			_obj = obj;
+		}
+
+		public void Deserialize(IXunitSerializationInfo info)
+		{
+			var jObject = new JObject();
+			foreach (var item in JObject.FromObject(_obj))
+				jObject.Add(item.Key, info.GetValue<string>(item.Key));
+			_obj = jObject.ToObject<T>();
+		}
+
+		public void Serialize(IXunitSerializationInfo info)
+		{
+			foreach (var item in JObject.FromObject(_obj))
+				info.AddValue(item.Key, item.Value.ToString(), typeof(string));
+		}
+
+		public static implicit operator T(JsonXunitSerializable<T> xunitSerializable)
+		{
+			return xunitSerializable._obj;
+		}
+
+		public static implicit operator JsonXunitSerializable<T>(T obj)
+		{
+			return new JsonXunitSerializable<T>(new T());
+		}
+	}
+}
